2a9133df5ebfb6a0cbc964465ab32a7a03c73b35
Kamaal ABOOTHALIB <kml5ive@gmail.com>
2023-06-05 22:08:02 +0530

    Chakra added
    Search Input added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import { describe, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MockedProvider } from "@apollo/client/testing";
+import userEvent from "@testing-library/user-event";
 import App from "./App.tsx";
 import { mocks } from "./mock";
 
@@ -21,5 +22,17 @@ describe("<App/>", () => {
       </MockedProvider>
     );
     expect(await screen.findByText("Hi")).toBeInTheDocument();
+  });
+
+  test("<App/> Should be able to type text", async () => {
+    const user = userEvent.setup();
+    render(
+      <MockedProvider>
+        <App />
+      </MockedProvider>
+    );
+    const input: HTMLInputElement = await screen.findByRole("search");
+    await user.type(input, "H");
+    expect(await input).toHaveValue("H");
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import "./App.css";
 import { useQuery } from "@apollo/client";
 import { GET_PODCASTS } from "./apollo";
+import { Input } from "@chakra-ui/react";
+import { ChangeEvent } from "react";
 
 function App() {
   const { loading } = useQuery(GET_PODCASTS);
   return (
-    <main role="app">{!loading ? <div>Hi</div> : <div>Loading...</div>}</main>
+    <main role="app">
+      <div>
+        <Input
+          role="search"
+          className="search__input"
+          onChange={async (e: ChangeEvent<HTMLInputElement>) => {
+            console.log(e);
+          }}
+        />
+      </div>
+      <div>{!loading ? <div>Hi</div> : <div>Loading...</div>}</div>
+    </main>
   );
 }
 
