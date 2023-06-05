9dbdc8fa4fd92ac8f4c5e81d0fffebca467c136c
Kamaal ABOOTHALIB <kml5ive@gmail.com>
2023-06-05 20:41:42 +0530

    test setup
    React test & vitest extended with vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./tests/setup.ts"],
+    globals: true,
+    coverage: {
+      provider: "c8",
+      thresholdAutoUpdate: true,
+      statements: 82.75,
+      branches: 96.77,
+      functions: 87.5,
+      lines: 82.75,
+    },
+  }
 })
