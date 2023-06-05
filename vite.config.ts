be5bfd2b294e5c00154337571cdf14c6fdc8b06d
Kamaal ABOOTHALIB <kml5ive@gmail.com>
2023-06-06 03:27:41 +0530

    Deploy script added

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/<REPO>/",
   plugins: [react()],
   test: {
     testTimeout: 10000,
