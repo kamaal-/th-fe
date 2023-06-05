f6bb9597bae0cc173bd72dab3c9387833b252743
Kamaal ABOOTHALIB <kml5ive@gmail.com>
2023-06-06 03:32:30 +0530

    Fix repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/<REPO>/",
+  base: "/th-fe/",
   plugins: [react()],
   test: {
     testTimeout: 10000,
